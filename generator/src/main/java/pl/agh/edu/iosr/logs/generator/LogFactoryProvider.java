9d9c1e9bee09072588fee7503b63cd7fa078da18
bryk <piotrek.bryk@gmail.com>
2014-04-11 19:44:11 +0200

    Migrated to Java 6

--- generator/src/main/java/pl/agh/edu/iosr/logs/generator/LogFactoryProvider.java
+++ generator/src/main/java/pl/agh/edu/iosr/logs/generator/LogFactoryProvider.java
@@ -8,14 +8,19 @@ import org.apache.log4j.Level;
 
 public class LogFactoryProvider {
 	public static LogFactory createLogFactory(Level level, InputStream messagesStream) {
-		ArrayList<String> messages = new ArrayList<>();
-		try (Scanner scanner = new Scanner(messagesStream)) {
+		ArrayList<String> messages = new ArrayList<String>();
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(messagesStream);
 			while (scanner.hasNextLine()) {
 				String msg = scanner.nextLine().trim();
 				if (msg.length() > 3) {
 					messages.add(msg);
 				}
 			}
+		} finally {
+			if (scanner != null)
+				scanner.close();
 		}
 		return new LogFactory(level, messages);
 	}
