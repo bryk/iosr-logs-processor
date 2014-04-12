90150f15d2659671e5d7caf305e9ffef4b9cc438
bryk <piotrek.bryk@gmail.com>
2014-04-12 05:00:48 -0700

    Added Kafka and Cloudera VM configuration scripts

--- bin/configure.sh
+++ bin/configure.sh
@@ -2,6 +2,15 @@
 # Configures Cloudera VM for the project
 
 echo "Configuring..."
-
+sudo yum update -y
+(
+  cd `dirname $0`
+  cd ..
+  rm -rf downloads/
+  mkdir -p downloads/
+  wget -O downloads/kafka.tgz http://www.eu.apache.org/dist/kafka/0.8.1/kafka_2.9.2-0.8.1.tgz
+  (cd downloads && tar xvfz kafka.tgz)
+  mv downloads/kafka_2.9.2-0.8.1/ downloads/kafka
+)
 echo "Done..."
 
