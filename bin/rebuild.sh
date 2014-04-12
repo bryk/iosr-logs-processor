43fdeef572c8fd96fde0732533be1c75f0f4cac4
bryk <piotrek.bryk@gmail.com>
2014-04-12 04:38:11 -0700

    Added kafka scripts and fixed infinte logging loop

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -2,6 +2,10 @@
 # Rebuilds all components needed for the project
 
 echo "Rebuilding..."
-
+(
+  cd `dirname $0`;
+  cd ../generator;
+  mvn clean package;
+)
 echo "Done..."
 
--- bin/run-logs-producer.sh
+++ bin/run-logs-producer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Runs logs producer
 
+(
+  cd "`dirname $0`/..";
+  java -jar generator/target/*-with-dependencies.jar
+)
+
