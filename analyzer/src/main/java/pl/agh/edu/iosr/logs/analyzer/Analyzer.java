584d405d393197ffdff3961740169d3b3e0f7aa6
bryk <piotrek.bryk@gmail.com>
2014-04-22 16:53:42 +0200

    Initialized analyzer project to proper default values

---
+++ analyzer/src/main/java/pl/agh/edu/iosr/logs/analyzer/Analyzer.java
@@ -0,0 +1,10 @@
+package pl.agh.edu.iosr.logs.analyzer;
+
+/**
+ * Analyzer program entry point.
+ */
+public class Analyzer {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
