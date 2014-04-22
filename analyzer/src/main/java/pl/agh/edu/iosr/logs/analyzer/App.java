5e96634b1bda417caf05f065e90f34a4cbf8624c
bryk <piotrek.bryk@gmail.com>
2014-04-22 16:44:19 +0200

    Created analyzer project from quickstart archetype.

---
+++ analyzer/src/main/java/pl/agh/edu/iosr/logs/analyzer/App.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.iosr.logs.analyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
