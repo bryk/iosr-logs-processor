78ab4ae285584d988a1bd4748e73cd43e39fcc1e
bryk <piotrek.bryk@gmail.com>
2014-05-05 09:07:38 -0700

    Updated kafka config

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Configures Cloudera VM for the project
+KAFKA_URL=https://archive.apache.org/dist/kafka/0.8.1/kafka_2.9.2-0.8.1.tgz
 
 echo "Configuring..."
 sudo yum update -y
@@ -8,7 +9,7 @@ sudo yum update -y
   cd ..
   rm -rf downloads/
   mkdir -p downloads/
-  wget -O downloads/kafka.tgz http://www.eu.apache.org/dist/kafka/0.8.1/kafka_2.9.2-0.8.1.tgz
+  wget -O downloads/kafka.tgz ${KAFKA_URL}
   (cd downloads && tar xvfz kafka.tgz)
   mv downloads/kafka_2.9.2-0.8.1/ downloads/kafka
 )
