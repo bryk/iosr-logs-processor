6d6facb73f50aa7a0bab08ec701698acb79386e2
Daniel Olszowski <olszowski.daniel@gmail.com>
2014-05-10 14:37:38 -0700

    Removed files which belongs to consumer project.

--- analyzer/src/main/java/pl/agh/edu/iosr/logs/writer/IWriter.java
+++
@@ -1,8 +0,0 @@
-package pl.agh.edu.iosr.logs.writer;
-
-import java.io.IOException;
-
-public interface IWriter {
-	void writeToFile(String path, String something) throws IOException;
-	void appendToFile(String path, String something) throws IOException;
-}
