a74920a07e790adab513f0e839bec06f12c548ea
Daniel Olszowski <olszowski.daniel@gmail.com>
2014-05-20 05:11:24 -0700

    Proper exception handling while writing to hdfs.

--- consumer/src/main/java/pl/agh/edu/iosr/logs/writer/hdfs/HDFSWriter.java
+++ consumer/src/main/java/pl/agh/edu/iosr/logs/writer/hdfs/HDFSWriter.java
@@ -1,6 +1,8 @@
 package pl.agh.edu.iosr.logs.writer.hdfs;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
@@ -8,29 +10,60 @@ import org.apache.hadoop.fs.Path;
 
 import pl.agh.edu.iosr.logs.writer.IWriter;
 
-
 public class HDFSWriter implements IWriter {
 
 	private FileSystem hdfs;
-	
+
 	public HDFSWriter(FileSystem hdfsFileSystem) {
 		hdfs = hdfsFileSystem;
 	}
-	
+
 	@Override
-	public void writeToFile(String path, String something) throws IOException {
-		FSDataOutputStream stream = hdfs.create(new Path(path));
-		stream.writeChars(something);
-		stream.flush();
-		stream.close();
+	public void writeToFile(String path, String something) {
+		FSDataOutputStream stream = null;
+		try {
+			stream = hdfs.create(new Path(path));
+		} catch (IllegalArgumentException e1) {
+			System.err.println(e1);
+		} catch (IOException e1) {
+			System.err.println(e1);
+		}
+		Writer writer = new OutputStreamWriter(stream);
+		try {
+			writer.write(something);
+		} catch (IOException e) {
+			System.err.println("Write error: " + e);
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				System.err.println("Close error: " + e);
+			}
+		}
 	}
 
 	@Override
-	public void appendToFile(String path, String something) throws IOException {
-		FSDataOutputStream stream = hdfs.append(new Path(path));
-		stream.writeChars(something);
-		stream.flush();
-		stream.close();
+	public void appendToFile(String path, String something) {
+		FSDataOutputStream stream = null;
+		try {
+			stream = hdfs.append(new Path(path));
+		} catch (IllegalArgumentException e1) {
+			System.err.println(e1);
+		} catch (IOException e1) {
+			System.err.println(e1);
+		}
+		Writer writer = new OutputStreamWriter(stream);
+		try {
+			writer.write(something);
+		} catch (IOException e) {
+			System.err.println("Write error: " + e);
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				System.err.println("Close error: " + e);
+			}
+		}
 	}
 
 }
