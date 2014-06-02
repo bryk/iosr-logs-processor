cd82504c5fa728d5e6f893e2cbc50591c8815b5a
Daniel Olszowski <olszowski.daniel@gmail.com>
2014-06-02 16:22:25 -0700

    Changed data format and data source folder

--- bin/init-test-data.sh
+++ bin/init-test-data.sh
@@ -3,17 +3,18 @@
 
 (
   cd `dirname $0`;
-  DIR="/tmp/testdb"
+  DIR="/user/cloudera/kafka"
+  #DIR="/tmp/testdb"
   DATA="../conf/test_data.csv"
   TABLE_NAME="iosr"
 
-  hadoop fs -rm -r -f $DIR
-  hadoop fs -mkdir $DIR
-  hadoop fs -put $DATA $DIR
+  #hadoop fs -rm -r -f $DIR
+  #hadoop fs -mkdir $DIR
+  #hadoop fs -put $DATA $DIR
 
   BEE="beeline -u jdbc:hive2://localhost:10000 -n cloudera -p cloudera -d org.apache.hive.jdbc.HiveDriver"
 
   $BEE -e "DROP TABLE ${TABLE_NAME};"
-  $BEE -e "create external table ${TABLE_NAME} (timestamp_millis TIMESTAMP COMMENT \"Timestamp of this log entry\", class STRING COMMENT \"Class issuing log\", level STRING COMMENT \"Log level\", message STRING COMMENT \"Log message\") row format delimited fields terminated by \",\" stored as textfile location \"${DIR}\";";
+  $BEE -e "create external table ${TABLE_NAME} (timestamp_millis TIMESTAMP COMMENT \"Timestamp of this log entry\", class STRING COMMENT \"Class issuing log\", level STRING COMMENT \"Log level\", message STRING COMMENT \"Log message\") row format delimited fields terminated by \"\001\" stored as textfile location \"${DIR}\";";
 )
 
